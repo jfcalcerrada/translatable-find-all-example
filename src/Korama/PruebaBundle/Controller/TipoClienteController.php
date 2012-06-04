e55b43cafd57abf119533e195f59f6f26575c5a3
Lluis Toyos <ltoyos@gmail.com>
2012-06-04 21:08:27 +0200

    Error en findTranslations

--- src/Korama/PruebaBundle/Controller/TipoClienteController.php
+++ src/Korama/PruebaBundle/Controller/TipoClienteController.php
@@ -9,8 +9,19 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class TipoClienteController extends Controller
-{
-    
+{    
+    public function indexAction()
+    {
+            $listatipoCliente = $this->getDoctrine()
+            ->getRepository('KoramaPruebaBundle:TipoCliente')
+            ->findAll();
+
+        return $this->render('KoramaPruebaBundle:TipoCliente:index.html.twig', 
+                array(
+                    'listatipoCliente' => $listatipoCliente
+                )
+                );
+    }
     public function newAction(Request $request)
     {
         $em = $this->getDoctrine()->getEntityManager();
@@ -36,6 +47,25 @@ class TipoClienteController extends Controller
     }
     public function savedAction(){
         return new Response('<html><body><h1>SAVED</h1></body></html>');
+    }
+    public function viewAction($id =0){
+        $em = $this->getDoctrine()->getEntityManager();
+        
+        $tipoCliente = $this->getDoctrine()
+        ->getRepository('KoramaPruebaBundle:TipoCliente')
+        ->find($id);
+        
+        
+        if (!$tipoCliente) {
+            throw $this->createNotFoundException('No product found for id '.$id);
+        } 
+        $repository = $em->getRepository('Gedmo\Translatable\Entity\Translation');
+        $translations = $repository->findTranslations($tipoCliente);
+        return $this->render('KoramaPruebaBundle:TipoCliente:view.html.twig', array(
+            'translations' => $translations
+        ));
+       
+        
     }
     
 }
